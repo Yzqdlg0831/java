14504e88724d4ba6fc0291a42892f76fce23a67b
Bo <1980697614@qq.com>
2022-03-29 23:14:50 +0800

    20220329

--- 20220329/src/TextDemo/TextDemo.java
+++ 20220329/src/TextDemo/TextDemo.java
@@ -1,5 +1,18 @@
 package TextDemo;
 
 public class TextDemo {
-
+    public static boolean fun(String str){
+        for(int i = 0;i < str.length();i++){
+            char a = str.charAt(i);
+            if(a > '9' || a < '0'){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "123456789";
+        boolean bool = fun(str);
+        System.out.println(bool);
+    }
 }
